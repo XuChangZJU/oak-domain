ac88cb399a68788a843041b6d4dc133df1e5b578
Xc@centOs <chinawraith@163.com>
2022-03-22 15:21:20 +0800

    移除src-base-domain

--- src/base-domain/Token/Action.ts
+++
@@ -1,5 +0,0 @@
-import { AbleAction } from "../../actions/action";
-import { ActionDef } from "../../types/Action";
-import { GenericAction } from "../../actions/action";
-export type ParticularAction = AbleAction;
-export type Action = GenericAction | ParticularAction;
