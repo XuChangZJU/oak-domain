399aca66dae4105e45c7d8a003e3e9c02a39d68b
Xc@centOs <chinawraith@163.com>
2022-09-10 10:30:11 +0800

    漏了一个编译

--- lib/checkers/index.d.ts
+++ lib/checkers/index.d.ts
@@ -1,3 +1,3 @@
 import { EntityDict } from '../base-app-domain';
 import { StorageSchema, EntityDict as BaseEntityDict, Context } from '../types';
-export declare function createCheckers<ED extends EntityDict & BaseEntityDict, Cxt extends Context<ED>>(schema: StorageSchema<ED>): import("../types").Checker<ED, keyof ED, Cxt>[];
+export declare function createDynamicCheckers<ED extends EntityDict & BaseEntityDict, Cxt extends Context<ED>>(schema: StorageSchema<ED>): import("../types").Checker<ED, keyof ED, Cxt>[];
--- src/checkers/index.ts
+++ src/checkers/index.ts
@@ -2,6 +2,6 @@ import { EntityDict } from '../base-app-domain';
 import { createModiRelatedCheckers } from '../store/modi';
 import { StorageSchema, EntityDict as BaseEntityDict, Context } from '../types';
 
-export function createCheckers<ED extends EntityDict & BaseEntityDict, Cxt extends Context<ED>>(schema: StorageSchema<ED>){
+export function createDynamicCheckers<ED extends EntityDict & BaseEntityDict, Cxt extends Context<ED>>(schema: StorageSchema<ED>){
     return createModiRelatedCheckers<ED, Cxt>(schema);
 }
