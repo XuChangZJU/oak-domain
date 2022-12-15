15f6ca662385ba989a9492feaf9f68566c7959fb
Xc <chinawraith@163.com>
2022-12-15 10:31:50 +0800

    微调了timer的定义

---
+++ src/types/Timer.ts
@@ -0,0 +1,16 @@
+import { RecurrenceRule, RecurrenceSpecDateRange, RecurrenceSpecObjLit } from 'node-schedule';
+import { EntityDict } from './Entity';
+import { AsyncContext } from "../store/AsyncRowStore";
+
+type RoutineFn<ED extends EntityDict, Cxt extends AsyncContext<ED>> = (context: Cxt) => Promise<string>;
+
+export type Routine<ED extends EntityDict, Cxt extends AsyncContext<ED>> = {
+    name: string;
+    fn: RoutineFn<ED, Cxt>;
+};
+
+export type Timer<ED extends EntityDict, Cxt extends AsyncContext<ED>> = {
+    name: string;
+    cron: RecurrenceRule | RecurrenceSpecDateRange | RecurrenceSpecObjLit | Date | string | number;
+    fn: RoutineFn<ED, Cxt>;
+};
