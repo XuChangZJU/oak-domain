42fca4aa3f6321a4df9d30d0e2614be143abfe66
Xc@centOs <chinawraith@163.com>
2023-02-27 17:06:08 +0800

    simpleConnector处理了buffer类型的返回

--- src/utils/SimpleConnector.ts
+++ src/utils/SimpleConnector.ts
@@ -101,7 +101,7 @@ export class SimpleConnector<ED extends EntityDict, BackCxt extends AsyncContext
     }
     
     serializeResult(result: any, context: BackCxt, headers: IncomingHttpHeaders, body: any): { body: any; headers?: Record<string, any> | undefined; } {
-        if (result instanceof Stream) {
+        if (result instanceof Stream || result instanceof Buffer) {
             return {
                 body: result,
             };
