9e680a92fe5f62960a74ae894f6b182b96ee2c42
wenjiarui <wenjiarui1@126.com>
2023-03-30 19:40:49 +0800

    vaccumOperOption类型export出来

--- lib/timers/oper.d.ts
+++ lib/timers/oper.d.ts
@@ -1,7 +1,7 @@
 import { EntityDict } from '../types/Entity';
 import { EntityDict as BaseEntityDict } from '../base-app-domain';
 import { AsyncContext } from '../store/AsyncRowStore';
-declare type VaccumOperOption<ED extends EntityDict & BaseEntityDict> = {
+export declare type VaccumOperOption<ED extends EntityDict & BaseEntityDict> = {
     aliveLine: number;
     excludeOpers?: {
         [T in keyof ED]?: ED[T]['Action'][];
@@ -16,4 +16,3 @@ declare type VaccumOperOption<ED extends EntityDict & BaseEntityDict> = {
  * @returns
  */
 export declare function vaccumOper<ED extends EntityDict & BaseEntityDict, Cxt extends AsyncContext<ED>>(option: VaccumOperOption<ED>, context: Cxt): Promise<void>;
-export {};
