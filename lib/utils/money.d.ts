dcb936b27d986fd769e642e0ec470e1af473d834
wangwenchen <920960949@qq.com>
2023-06-12 18:00:10 +0800

    千位计数法加逗号

--- lib/utils/money.d.ts
+++ lib/utils/money.d.ts
@@ -2,4 +2,5 @@ declare const ToCent: (float: number) => number;
 declare const ToYuan: (int: number) => number;
 declare const StringToCent: (value: string, allowNegative?: true) => number | undefined;
 declare const CentToString: (value: number) => string | undefined;
-export { ToCent, ToYuan, StringToCent, CentToString, };
+declare const ThousandCont: (value: number) => string | undefined;
+export { ToCent, ToYuan, StringToCent, CentToString, ThousandCont };
