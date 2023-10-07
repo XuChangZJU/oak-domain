158f21e706696f13d22f2428ae2e2481a6d0ed3e
Xc@centOs <chinawraith@163.com>
2023-10-07 17:48:21 +0800

    修正了port的声明

--- lib/types/Port.d.ts
+++ lib/types/Port.d.ts
@@ -1,18 +1,18 @@
 import { AsyncContext } from "../store/AsyncRowStore";
 import { EntityDict } from "./Entity";
-export type Exportation<ED extends EntityDict, T extends keyof ED, K extends string> = {
+export type Exportation<ED extends EntityDict, T extends keyof ED, K extends string, Cxt extends AsyncContext<ED>> = {
     name: string;
     id: string;
     entity: T;
     projection: ED[T]['Selection']['data'];
     headers?: K[];
     makeHeaders?: (dataList: Partial<ED[T]['Schema']>[]) => string[];
-    fn: (data: ED[T]['Schema'], context?: AsyncContext<ED>, properties?: Record<string, any>) => Promise<Partial<Record<string, string | number | boolean | null>>> | Partial<Record<string, string | number | boolean | null>>;
+    fn: (data: ED[T]['Schema'], context?: Cxt, properties?: Record<string, any>) => Promise<Partial<Record<string, string | number | boolean | null>>> | Partial<Record<string, string | number | boolean | null>>;
 };
-export type Importation<ED extends EntityDict, T extends keyof ED, K extends string> = {
+export type Importation<ED extends EntityDict, T extends keyof ED, K extends string, Cxt extends AsyncContext<ED>> = {
     name: string;
     id: string;
     entity: T;
     headers: K[];
-    fn: (data: Partial<Record<K, string | number | boolean>>[], context: AsyncContext<ED>, option?: Record<string, any>) => Promise<ED[T]['CreateMulti']['data']>;
+    fn: (data: Partial<Record<K, string | number | boolean>>[], context: Cxt, option?: Record<string, any>) => Promise<ED[T]['CreateMulti']['data']>;
 };
