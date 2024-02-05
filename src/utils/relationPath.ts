6ffb16409f54c4373ae95b656e32b2109bb5b8be
Xc <chinawraith@163.com>
2024-02-05 21:20:03 +0800

    把destructRelationPath分割成了两个函数

--- src/utils/relationPath.ts
+++ src/utils/relationPath.ts
@@ -6,11 +6,27 @@ export function destructRelationPath<ED extends EntityDict & BaseEntityDict, T e
     schema: StorageSchema<ED>,
     entity: T, 
     path: string, 
-    relationFilter?: ED['userRelation']['Selection']['filter'], 
+    relationFilter: ED['userRelation']['Selection']['filter'], 
     recursive?: boolean
     ): {
         projection: ED[T]['Selection']['data'];
         getData: (d: Partial<ED[T]['Schema']>) => ED['userRelation']['Schema'][];
     } {
         throw new Error('method not implemented');
+}
+
+export function destructDirectPath<ED extends EntityDict & BaseEntityDict, T extends keyof ED> (
+    schema: StorageSchema<ED>,
+    entity: T,
+    path: string,
+    recursive?: boolean
+): {
+    projection: ED[T]['Selection']['data'];
+    getData: (d: Partial<ED[T]['Schema']>) => {
+        entity: keyof ED,
+        entityId: string,
+        userId: string,
+    }[];
+} {
+    throw new Error('method not implemented');
 }
