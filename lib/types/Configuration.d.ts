50a47f936ef80fe282ad7fa316e9bac42fb5ac79
Xc@centOs <chinawraith@163.com>
2024-02-07 13:32:10 +0800

    Configuration设计(未完成)

---
+++ lib/types/Configuration.d.ts
@@ -0,0 +1,30 @@
+/**
+ *  后台环境配置
+ */
+export type ServerConfiguration = {
+    database: {
+        type: 'mysql';
+        host: string;
+        database: string;
+        port: number;
+        user: string;
+        password?: string;
+        connectionLimit: number;
+        charset: "utf8mb4_general_ci";
+    };
+    http: {
+        port: number;
+    };
+};
+/**
+ * 前后台共用的配置
+ */
+export type ProjectConfiguration = {
+    routerPrefixes?: {
+        aspect?: string;
+        endpoint?: string;
+        subscribe?: string;
+        getSubscribePoint?: string;
+        bridge?: string;
+    };
+};
